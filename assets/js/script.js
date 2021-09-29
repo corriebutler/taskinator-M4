7b3bfb0574808b6ffd7c47a4a8774042ac752793
Corrie Butler <corriebutler12@gmail.com>
2021-09-28 20:48:23 -0600

    added functionality to the delete button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var pageContentEl = document.querySelector('#page-content');
 var taskIdCounter = 0;
 var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
@@ -11,7 +12,7 @@ var taskFormHandler = function(event) {
     if (!taskNameInput || !taskTypeInput) {
         alert("You need to fill out the task form!");
         return false;
-    }
+    };
 
     formEl.reset();
 
@@ -85,11 +86,26 @@ var createTaskEl = function (taskDataObj) {
 
         // append to select
         statusSelectEl.appendChild(statusOptionEl);
-    }
+    };
 
     actionContainerEl.appendChild(statusSelectEl);
 
     return actionContainerEl;
-  }
+  };
+
+  var taskButtonHandler = function(event) {
+    console.log(event.target);
+
+    if (event.target.matches('.delete-btn')) {
+        var taskId = event.target.getAttribute('data-task-id');
+        deleteTask(taskId);
+    }
+  };
   
+  var deleteTask = function(taskId) {
+      var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+      taskSelected.remove();
+  };
+
   formEl.addEventListener("submit", taskFormHandler);
+  pageContentEl.addEventListener('click', taskButtonHandler);
